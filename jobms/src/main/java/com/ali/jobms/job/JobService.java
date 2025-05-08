25ba959821f3699cd8beafc4d44a59151854b419
ali-z93 <ali.eghmazi1993@gmail.com>
2025-05-08 10:51:07 -0400

    Job Microservice

---
+++ jobms/src/main/java/com/ali/jobms/job/JobService.java
@@ -0,0 +1,18 @@
+package com.ali.jobms.job;
+
+import java.util.List;
+
+public interface JobService {
+
+	List<Job> findAll();
+
+	void createJob(Job job);
+	
+	Job getJobByID(long id);
+
+	boolean deleteJobById(long id);
+
+	boolean updateJob(long id, Job updatedJob);
+	
+
+}
---
+++ jobms/src/main/java/com/ali/jobms/job/JobServiceImpl/JobServiceImpl.java
@@ -0,0 +1,70 @@
+package com.ali.jobms.job.JobServiceImpl;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.ali.jobms.job.Job;
+import com.ali.jobms.job.JobRepository;
+import com.ali.jobms.job.JobService;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+	JobRepository jobRepository;
+
+	public JobServiceImpl(JobRepository jobRepository) {
+		this.jobRepository = jobRepository;
+	}
+
+
+	@Override
+	public List<Job> findAll() {
+
+		return jobRepository.findAll();
+	}
+
+	@Override
+	public void createJob(Job job) {
+		jobRepository.save(job);
+
+	}
+
+	@Override
+	public Job getJobByID(long id) {
+		// TODO Auto-generated method stub
+		return jobRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public boolean deleteJobById(long id) {
+		try {
+			jobRepository.deleteById(id);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+
+	}
+
+	@Override
+	public boolean updateJob(long id, Job updatedJob) {
+		// TODO Auto-generated method stub
+
+		Optional<Job> jobOptional = jobRepository.findById(id);
+		if (jobOptional.isPresent()) {
+			Job job = jobOptional.get();
+			job.setTitle(updatedJob.getTitle());
+			job.setDescription(updatedJob.getDescription());
+			job.setMaxSalary(updatedJob.getMaxSalary());
+			job.setMinSalary(updatedJob.getMinSalary());
+			job.setLocation(updatedJob.getLocation());
+			jobRepository.save(job);
+			return true;
+		}
+
+		return false;
+	}
+}
