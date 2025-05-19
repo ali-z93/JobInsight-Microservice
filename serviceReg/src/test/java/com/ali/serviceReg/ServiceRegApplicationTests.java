faba786a1a453809ae104972c993b509c50d233b
ali-z93 <ali.eghmazi1993@gmail.com>
2025-05-19 11:05:40 -0400

    service registry

---
+++ serviceReg/src/test/java/com/ali/serviceReg/ServiceRegApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ali.serviceReg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
